6f90af86a281e96fc072aaae69f820fc33915666
vvchldmsdn@google.com <vvchldmsdn@gmail.com>
2022-11-22 16:07:41 +0900

    feat: 지역, 사건, 인물 모델 생성

--- backend/features/models.py
+++ backend/features/models.py
@@ -9,3 +9,33 @@ class Character(models.Model):
     description = models.TextField()
     age = models.IntegerField(default=0)
     image = models.TextField()
+
+
+class Family(models.Model):
+    upper = models.ForeignKey(
+        Character,
+        on_delete=models.CASCADE,
+        related_name='family_upper'
+    )
+    under = models.ForeignKey(
+        Character,
+        on_delete=models.CASCADE,
+        related_name='family_under'
+    )
+
+
+class Incident(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    participant = models.ManyToManyField(
+        Character, related_name='incident_character')
+
+
+class Region(models.Model):
+    name = models.CharField(max_length=20)
+    location_lat = models.FloatField()
+    location_lng = models.FloatField()
+    description = models.TextField()
+    image = models.TextField()
+    passed = models.ManyToManyField(Character, related_name='region_character')
+    happen = models.ManyToManyField(Incident, related_name='region_incident')
