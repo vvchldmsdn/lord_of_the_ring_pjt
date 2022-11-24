455afcf08177b4a153b210f3921f7e61f3bac670
vvchldmsdn@google.com <vvchldmsdn@gmail.com>
2022-11-24 14:28:40 +0900

    feat: orc, other objectification

--- backend/features/urls.py
+++ backend/features/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.character_list),
-    path('/race/<str:race_name>', views.get_race_list),
-    path('/character/<str:character_name>', views.get_character_detail)
+    path('race/<str:race_name>', views.get_race_list),
+    path('character/<str:character_name>', views.get_character_detail)
 ]
--- backend/features/views.py
+++ backend/features/views.py
@@ -17,16 +17,16 @@ def character_list(request):
         humans = get_list_or_404(Character, race='human')
         dwarves = get_list_or_404(Character, race='dwarf')
         hobbits = get_list_or_404(Character, race='hobbit')
-        orcs = get_list_or_404(Character, race='orc')
-        others = get_list_or_404(Character, race='other')
+        orcs = get_object_or_404(Character, race='orc')
+        others = get_object_or_404(Character, race='other')
 
         valars_serializer = CharacterSerializer(valars, many=True)
         elves_serializer = CharacterSerializer(elves, many=True)
         humans_serializer = CharacterSerializer(humans, many=True)
         dwarves_serializer = CharacterSerializer(dwarves, many=True)
         hobbits_serializer = CharacterSerializer(hobbits, many=True)
-        orcs_serializer = CharacterSerializer(orcs, many=True)
-        othres_serializer = CharacterSerializer(others, many=True)
+        orcs_serializer = CharacterSerializer(orcs)
+        othres_serializer = CharacterSerializer(others)
 
         valars_data = valars_serializer.data
         elves_data = elves_serializer.data
@@ -38,7 +38,7 @@ def character_list(request):
 
         response_data = {"valars": valars_data, "elves": elves_data, "humans": humnas_data,
                          "dwarves": dwarves_data, "hobbits": hobbits_data, "orcs": orcs_data, "others": others_data}
-        return Response(valars_serializer.data)
+        return Response(response_data)
 
     def post_characters():
         serializer = CharacterSerializer(data=request.data)
@@ -52,12 +52,26 @@ def character_list(request):
         return post_characters()
 
 
-def get_race_list(request, race):
-    races = get_list_or_404(Character, race=race)
-    serializer = CharacterSerializer(races, many=True)
-    return Response(serializer.data)
+@api_view(['GET'])
+def get_race_list(request, race_name):
 
+    def tmp_one():
+        races = get_list_or_404(Character, race=race_name)
+        serializer = CharacterSerializer(races, many=True)
+        return Response(serializer.data)
 
+    def tmp_two():
+        race = get_object_or_404(Character, race=race_name)
+        serializer = CharacterSerializer(race)
+        return Response(serializer.data)
+
+    if race_name in ['valar', 'human', 'elf', 'dwarf', 'hobbit']:
+        return tmp_one()
+    else:
+        return tmp_two()
+        
+
+@api_view(['GET'])
 def get_character_detail(request, character):
     character = get_object_or_404(Character, name=character)
     serializer = CharacterSerializer(character)
